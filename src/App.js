f7bd36c4b36145e52c9dff042eb489d062a81024
Jegathish-16 <jegathish162004@gmail.com>
2023-06-12 14:27:24 +0530

    Add. HolidayComponent thorugh axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import HolidayComponent from './Components/HolidayComponent/HolidayComponent';
 function App() {
   return (
     <div className="App">
-      
+      <HolidayComponent/>
     </div>
   );
 }
--- src/Components/HolidayComponent/HolidayComponent.jsx
+++ src/Components/HolidayComponent/HolidayComponent.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
+// import './HolidayComponent.css'
 export class HolidayComponent extends Component {
-  render() {
-    return (
-      <div>HolidayComponent</div>
-    )
-  }
-}
-
+    constructor(){
+        super()
+        this.state = {
+            holidays: [],
+            errorMessage: ''
+          };
+    }
+    componentDidMount() {
+        axios
+          .get('https://date.nager.at/api/v3/PublicHolidays/2023/AT')
+          .then(response => {
+            this.setState({ holidays: response.data });
+          })
+          .catch(error => {
+            console.log(error);
+            this.setState({ errorMessage: 'Error retrieving data' });
+          });
+      }
+    
+      render() {
+        const { holidays, errorMessage } = this.state;
+        return (
+          <React.Fragment>
+            <div className="title"><strong>Public Holidays</strong></div>
+            <div className="container">
+              {holidays.length ? (
+                holidays.map((holiday, index) => (
+                  <div key={index} className="box">
+                    <div className="holiday-details">
+                      <div className="holiday-date">Date: {holiday.date}</div>
+                      <div className="holiday-name">Name: {holiday.name}</div>
+                      <div className="holiday-localname">LocalName: {holiday.localName}</div>
+                      <div className="holiday-cntyname">CountryCode: {holiday.countryCode}</div>
+                    </div>
+                  </div>
+                ))
+              ) : null}
+              {errorMessage ? <div className="error">{errorMessage}</div> : null}
+            </div>
+          </React.Fragment>
+        );
+      }
+    }
+    
 export default HolidayComponent
