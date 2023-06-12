044f011b34fa20f8cf39a9625855423028a3fdf2
Jegathish-16 <jegathish162004@gmail.com>
2023-06-12 13:57:04 +0530

    Initial commit

---
+++ src/Components/HolidayComponent/HolidayComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class HolidayComponent extends Component {
+  render() {
+    return (
+      <div>HolidayComponent</div>
+    )
+  }
+}
+
+export default HolidayComponent
